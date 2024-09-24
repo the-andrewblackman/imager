884ec42c2e4d274b1bc92541fb8cb4d53e653367
the-andrewblackman <andrewronaldblackman@gmail.com>
2024-09-24 15:50:59 -0700

    added memoization for links

--- src/main/java/com/image/controller/ShowController.java
+++ src/main/java/com/image/controller/ShowController.java
@@ -1,18 +1,7 @@
 package com.image.controller;
 
-import com.image.entity.ImageEntity;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller  // Use @Controller for Thymeleaf templates
 public class ShowController {
--- src/main/java/com/image/controller/WebViewController.java
+++ src/main/java/com/image/controller/WebViewController.java
@@ -6,16 +6,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.function.Predicate;
 
 @Controller  // Use @Controller for Thymeleaf templates
 public class WebViewController {
